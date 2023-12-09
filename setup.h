60a1c897843993d8a1277bec958fd057c34864d2
Karel Kotrba <83918439+kkodl@users.noreply.github.com>
2023-12-09 20:16:18 +0100

    Add files via upload

---
+++ setup.h
@@ -0,0 +1,14 @@
+
+// WiFi credentials
+#define WIFI_PASS "_your_WiFi_password_"
+#define WIFI_SSID "_your_WiFi_SSID_"
+
+#define HTTP_GET_INTERVAL_SECS 180        //interval in secs to load data to Zivy Obraz, do not put less than 60s
+
+// PID Golemio server token here - see https://api.golemio.cz/api-keys/auth/sign-in
+String auth_token = "_fill in your Golemio API token here, it's long, don't loose part of it during copy paste_";
+
+// Zivy obraz
+String Server_Name = "http://in.zivyobraz.eu";        // server URL for "Zivy Obraz"
+String Server_Key = "/?import_key=zde vyplnte vas import token pro zivy obraz";  // your import token for "Zivy Obraz" https://zivyobraz.eu/
+
